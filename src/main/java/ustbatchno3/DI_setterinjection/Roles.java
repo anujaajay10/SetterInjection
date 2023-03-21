529f99e55582ff769307beac715093e86901bcd2
Anuja Ajay <anujaajay10@gmail.com>
2023-03-21 09:34:48 +0530

    21/03/2023

---
+++ src/main/java/ustbatchno3/DI_setterinjection/Roles.java
@@ -0,0 +1,27 @@
+package ustbatchno3.DI_setterinjection;
+
+public class Roles {
+	public String charactername;
+	public String responsibility;
+	public String getCharactername() {
+		return charactername;
+	}
+	public void setCharactername(String charactername) {
+		this.charactername = charactername;
+	}
+	public String getResponsibility() {
+		return responsibility;
+	}
+	public void setResponsibility(String responsibility) {
+		this.responsibility = responsibility;
+	}
+	@Override
+	public String toString() {
+		return "Roles [charactername=" + charactername + ", responsibility=" + responsibility + "]";
+	}
+	public void display()
+	{
+		System.out.println("My name is "+ charactername +" and my responsibility is " +responsibility);
+	}
+
+}
